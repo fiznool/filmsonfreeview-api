59c384c4fa5945319224e7a252be651385d5f18d
Tom Spencer <tom.spencer@studiozeffa.com>
2017-12-03 16:48:59 +0000

    API now handles dates and times as strings to prevent timezone issues.

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
         }
     },
     "rules": {
+        "no-console": 0,
         "indent": [
             "error",
             2
--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,11 @@
 create table filmsonfreeview.${film_table_name} (
-  id          serial primary key,
-  name        text not null,
-  year        int,
-  synopsis    text,
-  imdb_id     text,
-  imdb_rating text,
-  tmdb_id     text,
-  tmdb_rating int
+  id            serial primary key,
+  name          text not null,
+  year          int,
+  synopsis      text,
+  imdb_url      text,
+  tmdb_image_id text,
+  tmdb_rating   int
 );
 
 comment on table filmsonfreeview.${film_table_name} is 'A film shown on a Freeview channel.';
@@ -14,9 +13,8 @@ comment on column filmsonfreeview.${film_table_name}.id is 'The primary unique i
 comment on column filmsonfreeview.${film_table_name}.name is 'The name of the film.';
 comment on column filmsonfreeview.${film_table_name}.year is 'The year that the film was made.';
 comment on column filmsonfreeview.${film_table_name}.synopsis is 'A short description of the film.';
-comment on column filmsonfreeview.${film_table_name}.imdb_id is 'The ID of the IMDB record for the film.';
-comment on column filmsonfreeview.${film_table_name}.imdb_rating is 'The IMDB rating of the film.';
-comment on column filmsonfreeview.${film_table_name}.tmdb_id is 'The ID of TheMovieDB record for the film.';
+comment on column filmsonfreeview.${film_table_name}.imdb_url is 'The IMDB URL for the film.';
+comment on column filmsonfreeview.${film_table_name}.tmdb_image_id is 'The ID of TheMovieDB poster image for the film.';
 comment on column filmsonfreeview.${film_table_name}.tmdb_rating is 'TheMovieDB rating of the film.';
 
 
@@ -31,18 +29,22 @@ comment on column filmsonfreeview.${film_table_name}.tmdb_rating is 'TheMovieDB 
 
 
 create table filmsonfreeview.${showtime_table_name} (
-  id        serial primary key,
-  starts_at timestamp with time zone not null,
-  ends_at   timestamp with time zone not null,
-  film_id   integer not null references filmsonfreeview.${film_table_name}(id),
-  channel   text not null
+  id             serial primary key,
+  starts_at_date text not null,
+  starts_at_time text not null,
+  ends_at_date   text not null,
+  ends_at_time   text not null,
+  film_id        integer not null references filmsonfreeview.${film_table_name}(id),
+  channel        text not null
   -- channel_id integer not null references filmsonfreeview.${channel_table_name}(id)
 );
 
 comment on table filmsonfreeview.${showtime_table_name} is 'The time that a film is being shown on a certain channel on Freeview.';
 comment on column filmsonfreeview.${showtime_table_name}.id is 'The primary unique identifier for the showtime.';
-comment on column filmsonfreeview.${showtime_table_name}.starts_at is 'The start time for the showtime.';
-comment on column filmsonfreeview.${showtime_table_name}.ends_at is 'The end time for the showtime.';
+comment on column filmsonfreeview.${showtime_table_name}.starts_at_date is 'The start date for the showtime.';
+comment on column filmsonfreeview.${showtime_table_name}.starts_at_time is 'The start time for the showtime.';
+comment on column filmsonfreeview.${showtime_table_name}.ends_at_date is 'The end date for the showtime.';
+comment on column filmsonfreeview.${showtime_table_name}.ends_at_time is 'The end time for the showtime.';
 comment on column filmsonfreeview.${showtime_table_name}.film_id is 'The ID of the film being shown.';
 comment on column filmsonfreeview.${showtime_table_name}.channel is 'The channel that the film is being shown on.';
 -- comment on column filmsonfreeview.${showtime_table_name}.channel_id is 'The ID of the channel that the film is being shown on.';
